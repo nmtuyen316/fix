afb6b150ef3d30d935ca0439c23e721a20ee9b24
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-11-16 10:44:58 +0700

    Delete dbrepair.sql

--- myapp/db/dbrepair.sql
+++
@@ -1,9 +0,0 @@
---ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
---flush privileges;
-create database QLKH
-use QLKH
-create table KHACHHANG(
-	MAKH INT AUTO_INCREMENT PRIMARY KEY,
-    TEN NVARCHAR(50),
-    PASSWORD NVARCHAR(100)
-);
