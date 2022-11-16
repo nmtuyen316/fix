953cc606b544bbadfa8c346e0608a5b3006deefe
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-11-16 08:32:46 +0700

    add layout

--- myapp/app.js
+++ myapp/app.js
@@ -20,7 +20,7 @@ app.use(session({
 }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
---
+++ myapp/routes/signup.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var database = require('../db/db');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('signup', { session:req.session });
+});
+
+
+
+module.exports = router;
