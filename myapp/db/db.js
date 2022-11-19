d3e108eba961ecdfe135641c5728931527825fd9
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-11-19 14:22:37 +0700

    design db

--- myapp/db/db.js
+++ myapp/db/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:process.env.PASS
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASS
 });
 connection.connect(function(error){
     if(error){
--- myapp/routes/signup.js
+++ myapp/routes/signup.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var database = require('../db/db');
+// var database = require('../db/db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup', { session:req.session });
