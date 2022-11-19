bc29ad151a2608dd0c7dfb1b41ee908c6b9eccaf
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-11-19 15:11:41 +0700

    update

--- myapp/db/dbConfig.js
+++ myapp/db/dbConfig.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 module.exports={
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
-    password:process.env.MYSQL_PASS
+    password:process.env.MYSQL_PASS,
+    database:process.env.MYSQL_DATABASE
 }
