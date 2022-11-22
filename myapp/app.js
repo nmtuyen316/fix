2698525f1b98d2669ff7e8eb49d2d898323c31c7
nmtuyen316 <95425898+nmtuyen316@users.noreply.github.com>
2022-11-22 11:16:34 +0700

    login

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require("cors");
+require('dotenv').config();
 // //database
 // const dbconfig =require('./db/dbConfig');
 // const connection = require('./db/connection');
@@ -13,15 +14,15 @@ var logger = require('morgan');
 //   const results = await query(conn, 'SELECT * FROM tweets').catch(console.log);
 //   res.json({ results });
 // })
-
+const PORT = process.env.PORT || 3000;
 var session = require('express-session');
 //router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signinRouter = require('./routes/signin');
-var signupRouter = require('./routes/signup');
 
 var app = express();
+
 
 app.use(session({
   secret:'webslesson',
@@ -33,7 +34,11 @@ app.use(session({
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+var corsOptions = {
+  origin: `http://localhost:${PORT}`
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -43,7 +48,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signin', signinRouter);
-app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+//database
+const dbconfig = require('../db/dbConfig');
+const connection = require('../db/connection');
+const query = require('../db/query');
+//action
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EagleTealRepair - Sửa chữa & cài đặt phần mềm máy tính', session:req.session });
+router.get('/', async function(req, res, next) {
+  const conn = await connection(dbconfig).catch(e => {}) 
+  const results = await query(conn, 'SELECT name FROM Product where productID=1').catch(console.log);
+  res.render('index', { title: 'EagleTealRepair - Sửa chữa & cài đặt phần mềm máy tính',name:results, session:req.session });
 });
 
 module.exports = router;
